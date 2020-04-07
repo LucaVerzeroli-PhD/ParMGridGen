c9a61d6cfb41f4cd901ae4e36d58b5a80ea3ece2
Luca Verzeroli Dell <luca.verzeroli@unibg.it>
2020-04-07 08:59:25 +0200

    Copy of ParMGridgen

---
+++ mgridgen.h
@@ -0,0 +1,7 @@
+typedef int idxtype;
+typedef double realtype;
+
+extern "C" {
+void MGridGen(int, idxtype *, realtype *, realtype *, idxtype *, realtype *,
+              int, int, int *, int *, int *, idxtype *);
+}
